afda814e2eeb3a1f2fde152e16a72125180874f5
Daniel <itzdanielmichael@gmail.com>
2021-09-07 22:46:19 -0400

    having issue with firebase mock

---
+++ client/src/__tests__/Intro.spec.js
@@ -0,0 +1,28 @@
+import Enzyme from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import firebase from 'firebase';
+
+import Intro from './../screens/Intro';
+import IntroForms from './../components/IntroForms';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+//  having an issue mocking firebase
+jest.mock('firebase', () => {
+  const auth = jest.fn();
+  const mAuth = { signInWithRedirect: jest.fn() };
+  // @ts-ignore
+  auth.GoogleAuthProvider = jest.fn();
+  // @ts-ignore
+  auth.Auth = jest.fn(() => mAuth);
+  return { auth };
+});
+
+describe('<Intro />', () => {
+  it('renders 1 <Introforms /> components', () => {
+    const wrapper = shallow(<Intro />);
+    expect(wrapper.find(IntroForms)).to.have.lengthOf(1);
+  });
+});
---
+++ client/src/__tests__/test-utils/testUtils.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render as rtlRender } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Router, Route } from 'react-router-dom';
+import { CurrentUserProvider } from '../../context/currentUser';
+import { ScrimsProvider } from '../../context/scrimsContext';
+
+const history = createMemoryHistory();
+
+function render(ui, { preloadedState, ...renderOptions } = {}) {
+  function Wrapper({ children }) {
+    return (
+      <ScrimsProvider>
+        <Router history={history}>
+          <Route>{children}</Route>
+        </Router>
+      </ScrimsProvider>
+    );
+  }
+  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
+}
+
+// re-export everything
+export * from '@testing-library/react';
+// override render method
+export { render, history };
