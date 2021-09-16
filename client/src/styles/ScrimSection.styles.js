cff4a94f553592ec520c768a364eb0ba458fd251
Daniel <itzdanielmichael@gmail.com>
2021-09-16 17:10:09 -0400

    update App.js, ScrimSection.styles.js

--- client/src/styles/ScrimSection.styles.js
+++ client/src/styles/ScrimSection.styles.js
@@ -1,4 +1,4 @@
-import { makeStyles } from '@material-ui/core';
+import makeStyles from '@mui/styles/makeStyles';
 
 const BG_GIF =
   'https://pa1.narvii.com/5779/8d76b2b8112e6aa9494a93f0ca6bbffe96e2f6c3_hq.gif';
