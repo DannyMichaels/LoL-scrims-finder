fc5c6242a59261c67f6e6ae60db1f01d0fe1429b
Daniel Michael <itzdanielmichael@gmail.com>
2022-02-23 11:23:36 -0500

    Main (#83)
    * Profile image (#78)
    
    * Update ImageManipulator.js
    
    refactor
    
    * security updates
    
    * add server options backg
    
    * create ProfileImage component
    
    * add ProfileImage
    
    * add level to profile image
    
    * add fallback image
    
    * Use countdown hook (#80)
    
    * Update ImageManipulator.js
    
    refactor
    
    * security updates
    
    * add server options backg
    
    * Profile image (#77)
    
    * create ProfileImage component
    
    * add ProfileImage
    
    * add level to profile image
    
    * add fallback image
    
    * refactor countdowntimer component by moving countdown logic to a hook
    
    * refactor countdowntimer component by moving countdown logic to a hook
    
    * refactor hook
    
    * Use countdown hook (#79)
    
    * Profile image (#78)
    
    * Update ImageManipulator.js
    
    refactor
    
    * security updates
    
    * add server options backg
    
    * create ProfileImage component
    
    * add ProfileImage
    
    * add level to profile image
    
    * add fallback image
    
    * refactor countdowntimer component by moving countdown logic to a hook
    
    * refactor countdowntimer component by moving countdown logic to a hook
    
    * refactor hook
    
    * refactor hook
    
    * use my new words api for generating random lobby name
    
    * use new words api and add count button

--- client/src/services/wordsApi.js
+++ client/src/services/wordsApi.js
@@ -1,18 +1,33 @@
 import axios from 'axios';
 
-export const generateRandomLobbyName = async () => {
+export const generateRandomLobbyName = async (wordsCount = 2) => {
   try {
-    const promise1 = axios.get('https://random-word-api.herokuapp.com/word');
-    const promise2 = axios.get('https://random-word-api.herokuapp.com/word');
+    const randomWordUrl = `https://gitcat-words-api.herokuapp.com/api/words/random?count=${wordsCount}`;
 
-    const response = await Promise.all([promise1, promise2]);
+    const { data: words } = await axios.get(randomWordUrl);
 
-    // this api is returning an object which contains an array but the array will always have 1 object, Idk I was scratching my head.
-    const [{ data: first }, { data: second }] = response;
-
-    return `${first} ${second}`;
+    return words.join(' ');
   } catch (error) {
     console.error(error);
     throw error;
   }
 };
+
+// export const generateRandomLobbyName = async () => {
+//   try {
+//     const randomWordUrl = 'https://gitcat-words-api.herokuapp.com/api/word';
+
+//     const promise1 = axios.get(randomWordUrl);
+//     const promise2 = axios.get(randomWordUrl);
+
+//     const [{ data: first }, { data: second }] = await Promise.all([
+//       promise1,
+//       promise2,
+//     ]);
+
+//     return `${first} ${second}`;
+//   } catch (error) {
+//     console.error(error);
+//     throw error;
+//   }
+// };
